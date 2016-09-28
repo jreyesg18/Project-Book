fdf3c049cc1dff2496bb2eb256d07bfffc8a7381
javier <j.reyesg18@gmail.com>
2016-09-28 10:49:49 -0300

    modificacion

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ActiveRecord::Base
 
   validates :rating, :numericality => {:only_integer => true, :message => "Debe ser numero"}
   validates :comentario, length: { in: 5..100 , message: "Debe tener entre 5 y 20 caracteres"}
+  validates_inclusion_of :rating, :in => 1..7, :message => "La nota debe ser entre 1 a 7"
 
 end
